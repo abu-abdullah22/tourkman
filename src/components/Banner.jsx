7ba73b49450eb49a745a0ef14c5f37342cc6671e
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-04-26 01:58:41 +0600

    banner section more design

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ import { Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
 import 'animate.css';
 const Banner = () => {
     return (
-        <div className='my-4 bg-[#80665F] rounded-lg'>
+        <div className='my-4 bg-[#80665F] rounded-lg mx-10 lg:mx-0'>
              <Swiper
         effect={'coverflow'}
         grabCursor={true}
@@ -26,31 +26,31 @@ const Banner = () => {
         className="mySwiper lg:h-[88vh] w-[50vw]"
       >
         <SwiperSlide>
-          <img className='absolute contrast-[50%]' src="/public/banner1.jpg" />
-          <div className='absolute top-[20%] hidden md:block  md:top-[50%] left-[10%] bg-none md:left-[20%]'>
+          <img className='relative contrast-[50%]' src="/public/banner1.jpg" />
+          <div className='absolute top-[20%] hidden md:block lg:top-[50%] left-[10%] bg-none md:left-[20%]'>
                 <h1 className='text-white text-4xl font-bold animate__animated animate__backInDown my-8'> Gateway to Unforgettable Adventures</h1>
-                <p className='text-white md:w-2/3 lg:text-center font-semibold animate__animated animate__backInUp'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
+                <p className='text-white md:w-2/3 lg:text-center font-semibold animate__animated animate__backInUp lg:mx-10'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img className='absolute contrast-[50%]' src="/public/banner2.jpg" />
-          <div className='absolute top-[20%] hidden md:block  md:top-[50%] left-[10%] bg-none md:left-[20%]'>
+          <img className='relative contrast-[50%]' src="/public/banner2.jpg" />
+          <div className='absolute top-[20%] hidden md:block lg:top-[50%] left-[10%] bg-none md:left-[20%]'>
                 <h1 className='text-white text-4xl font-bold animate__animated animate__backInDown my-8'> Gateway to Unforgettable Adventures</h1>
-                <p className='text-white lg:text-center md:w-2/3 font-semibold'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
+                <p className='text-white lg:text-center md:w-2/3 font-semibold lg:mx-10'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img className='absolute contrast-[50%]' src="/public/banner3.jpg" />
-          <div className='absolute top-[20%] hidden md:block  md:top-[50%] left-[10%] bg-none md:left-[20%]'>
+          <img className='relative contrast-[50%]' src="/public/banner3.jpg" />
+          <div className='absolute top-[20%] hidden md:block lg:top-[50%]  md:top-[20%] left-[10%] bg-none md:left-[20%]'>
                 <h1 className='text-white text-4xl font-bold animate__animated animate__backInDown my-8'> Gateway to Unforgettable Adventures</h1>
-                <p className='text-white lg:text-center md:w-2/3 font-semibold'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
+                <p className='text-white lg:text-center md:w-2/3 font-semibold lg:mx-10'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img className='absolute contrast-[50%]' src="/public/banner4.jpg" />
-          <div className='absolute top-[20%] hidden md:block  md:top-[50%] left-[10%] bg-none md:left-[20%]'>
+          <img className='relative contrast-[50%]' src="/public/banner4.jpg" />
+          <div className='absolute top-[20%] hidden md:block lg:top-[50%] left-[10%] bg-none md:left-[20%]'>
                 <h1 className='text-white text-4xl font-bold animate__animated animate__backInDown my-8'> Gateway to Unforgettable Adventures</h1>
-                <p className='text-white lg:text-center md:w-2/3 font-semibold'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
+                <p className='text-white lg:text-center md:w-2/3 font-semibold lg:mx-10'>Discover breathtaking destinations and create lasting memories with our expertly curated travel experiences. Start your journey today!</p>
           </div>
         </SwiperSlide>
         </Swiper>
