81fea963e7537f6ba131151fa972eae43ad7a75a
abu-abdullah22 <rorschachmayhem@gmail.com>
2024-04-30 22:16:31 +0600

    fixed one more time

--- src/pages/AllTourist.jsx
+++ src/pages/AllTourist.jsx
@@ -14,19 +14,14 @@ const AllTourist = () => {
     }, [defaultSpots]);
 
     const handleSortSpot = (option) => {
-        let sortedData = [];
-        switch (option) {
-            case 'cost':
-                sortedData = defaultSpots.slice().sort((a, b) => a.cost - b.cost);
-                break;
-            case 'cost-desc':
-                sortedData = defaultSpots.slice().sort((a, b) => b.cost - a.cost);
-                break;
-            default:
-                sortedData = defaultSpots;
-                break;
+        if (option === 'cost') {
+            const sortedData = defaultSpots.slice().sort((a, b) => b.cost - a.cost);
+            SetSortedSpots(sortedData);
         }
-        SetSortedSpots(sortedData);
+        else {
+            SetSortedSpots(defaultSpots);
+        }
+
         setSortOption(option);
     }
     return (
@@ -34,16 +29,13 @@ const AllTourist = () => {
             <Helmet><title>All Tourist Spot</title></Helmet>
             <h2 className="text-4xl text-center my-12 italic">All Tourist Spots </h2>
             <details className="dropdown mt-12">
-                <summary className="m-1 btn bg-[#5489C8] text-white hover:bg-[#E46D30]"> {sortOption === 'cost' ? 'Cost' : 'Default'}</summary>
+                <summary className="m-1 btn bg-[#5489C8] text-white hover:bg-[#E46D30]">Sort By: {sortOption === 'cost' ? 'Cost' : 'Default'}</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                <li>
+                    <li>
                         <button onClick={() => handleSortSpot('default')}>Default</button>
                     </li>
                     <li>
-                        <button onClick={() => handleSortSpot('cost')}>Cost (Low to High)</button>
-                    </li>
-                    <li>
-                        <button onClick={() => handleSortSpot('cost-desc')}>Cost (High to Low)</button>
+                        <button onClick={() => handleSortSpot('cost')}>Cost [High to Low]</button>
                     </li>
                 </ul>
             </details>
